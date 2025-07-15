b42d8eff94fc37613aedca026d6f02b45eecb80f
Kyriakos Kartas <mail@kkartas.gr>
2025-07-15 17:22:05 +0300

    ci: add PyPI auto-publish workflow and rename package to apilinker

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 __version__ = "0.1.0"
 
 setup(
-    name="kkartas-apilinker",
+    name="apilinker",
     version=__version__,
     description="A universal bridge to connect, map, and automate data transfer between any two REST APIs",
     author="K. Kartas",
