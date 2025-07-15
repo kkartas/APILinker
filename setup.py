70926a08626633fec831932dd628f6a9003d90a6
Kyriakos Kartas <mail@kkartas.gr>
2025-07-15 18:01:28 +0300

    chore: bump version to 0.2.0 and add utf-8 encoding for README

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 setup(
     name="apilinker",
